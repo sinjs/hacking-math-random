e61e2829e4ca713c0d898f8b2455d476bd5b6f90
sinjs <50749166+sinjs@users.noreply.github.com>
2024-09-08 14:46:48 +0200

    Add challenge

---
+++ challenge/index.ts
@@ -0,0 +1,41 @@
+import readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output });
+
+console.log("Number Guessing Game");
+
+console.log(
+  "The game will print 5 numbers and you will have to guess the next number in the sequence. If you get the number right you win the game!"
+);
+
+console.log("\nYour lucky numbers are:");
+
+const initialLuckyNumbers = Array.from(Array(5), Math.random);
+
+for (const luckyNumber of initialLuckyNumbers) {
+  console.log(luckyNumber);
+}
+
+console.log("\nNow, time to guess the next number!");
+
+const numberGuess = await rl
+  .question("What is the next lucky number? ")
+  .then((response) => parseFloat(response));
+
+if (Number.isNaN(numberGuess)) {
+  console.error("Error: You entered an invalid number!");
+  process.exit(1);
+}
+
+const nextLuckyNumber = Math.random();
+
+if (numberGuess !== nextLuckyNumber) {
+  console.error(
+    "\nThat's the wrong number, the correct number was",
+    nextLuckyNumber
+  );
+  process.exit(1);
+}
+
+console.log('\nGood job and nice "guess", you won the game!');
---
+++ solution/main.py
@@ -0,0 +1,2 @@
+# Write your solution here to solve the problem, you are allowed to use any external packages
+# You are not allowed to change the challenge in any way.
